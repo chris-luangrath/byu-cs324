bd740c6dd942887b821d1108fd2659fcfcbab704
chris-luangrath <thechrisluangrath@gmail.com>
2022-09-23 19:45:27 -0600

    fixing seg fault

--- lab-shell-pipeline/tsh.c
+++ lab-shell-pipeline/tsh.c
@@ -124,7 +124,8 @@ void eval(char *cmdline)
     char *newenviron[] = { NULL };
 
     // only 1 thing
-    if(num_args == 0) {
+    printf("num_args: %d",num_args);
+    if(num_args <= 1) {
         if ((pid1 = fork()) < 0) {
             fprintf(stderr, "Could not fork()");
             exit(1);
