615bd9bac058990af9b37afd2cbf67d146faa58b
chris-luangrath <thechrisluangrath@gmail.com>
2022-09-24 18:40:08 -0600

    uhh closing stuff

--- lab-shell-pipeline/tsh.c
+++ lab-shell-pipeline/tsh.c
@@ -207,7 +207,7 @@ void eval(char *cmdline)
         // close(STDIN_FILENO)
         close(STDOUT_FILENO);
         close(p[0]);
-        close(p[1]);
+        // close(p[1]); -----------------
         
         execve(argv[cmds[0]],&argv[cmds[0]],newenviron); // TODO: i 
 
@@ -224,7 +224,7 @@ void eval(char *cmdline)
                 close(STDOUT_FILENO);
             }
             dup2(p[0],STDIN_FILENO);
-            close(p[0]);
+            // close(p[0]); -----------------
             close(p[1]);
             
             // Run the executable in the context of the child process using execve()
