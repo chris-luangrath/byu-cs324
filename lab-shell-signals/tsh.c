21d8e6f01157c91db4f92bb4dcb6565924806bee
chris-luangrath <thechrisluangrath@gmail.com>
2022-10-07 19:54:13 -0600

    huh sigchld handler was wrong

--- lab-shell-signals/tsh.c
+++ lab-shell-signals/tsh.c
@@ -457,8 +457,8 @@ void sigchld_handler(int sig)
     // }
     int status;
     while((pid = waitpid(-1,status, WNOHANG | WUNTRACED)) && pid != 0){
-        printf("hey it's breaking here\n");
-        fprintf(stderr, "hey it's breaking here\n");
+        // printf("hey it's breaking here\n");
+        // fprintf(stderr, "hey it's breaking here\n");
         // fprintf(stdout, "%s: %s\n", msg, strerror(errno));
         if(WIFSTOPPED(status)){
             // deletejob(jobs,pid);
@@ -471,7 +471,8 @@ void sigchld_handler(int sig)
         } else if(WIFEXITED(status)){
             deletejob(jobs,pid);
             // printf("the job has been terminated normally yo\n");
-
+        } else {
+            printf("nothing here....\n");
         }
     }
         
