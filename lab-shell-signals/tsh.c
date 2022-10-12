5e7ee099495a379af21e04a1af38a9035fc590b8
chris-luangrath <thechrisluangrath@gmail.com>
2022-10-11 22:59:34 -0600

    bgfg

--- lab-shell-signals/tsh.c
+++ lab-shell-signals/tsh.c
@@ -438,7 +438,7 @@ void do_bgfg(char **argv)
     }
     struct job_t *job;
     if((argv[1][0] == '%')){
-        if(job = getjobjid(jobs,atoi(argv[1]+1)) || job == NULL){
+        if(job = getjobjid(jobs,atoi(argv[1]+1))){
         // if(job = getjobjid(jobs,atoi(argv[1]+1))){
             printf("%%%d: No such job\n",atoi(argv[1]+1));
             return;
