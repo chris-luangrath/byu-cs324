fb83b44a68ddaa9e4e7a7225fea0799ee28f0032
chris-luangrath <thechrisluangrath@gmail.com>
2022-10-29 16:21:38 -0600

    uh

--- lab-socket-hunt/treasure_hunter.c
+++ lab-socket-hunt/treasure_hunter.c
@@ -77,7 +77,8 @@ int main(int argc, char *argv[]) {
 
 	// pre-socket
 
-	s = getaddrinfo((server), port_c, &hints, &result);
+	s = getaddrinfo(server, 10, &hints, &result);
+	// s = getaddrinfo(server, port_c, &hints, &result);
 	if (s != 0) {
 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
 		exit(EXIT_FAILURE);
