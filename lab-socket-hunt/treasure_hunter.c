5240d12b3aa2298f43bcc0f3f62c8a156f4c8136
chris-luangrath <thechrisluangrath@gmail.com>
2022-10-29 20:00:48 -0600

    it's kinda working?

--- lab-socket-hunt/treasure_hunter.c
+++ lab-socket-hunt/treasure_hunter.c
@@ -128,13 +128,13 @@ int main(int argc, char *argv[]) {
 	// unsigned short val = 0x0000000000000000;
 	int i = 0;
 	int n = 0;
-	printf("hey1\n")
+	printf("hey1\n");
 	memcpy(&n,&rec_buf[1], 4);
-	printf("hey2\n")
+	printf("hey2\n");
 	// bzero(buf, BUFSIZE);
 	char* nonce[4];
 	memcpy(&nonce,&rec_buf[n+4], 4);
-	printf("hey3\n")
+	printf("hey3\n");
 	printf("%x\n", ntohs(nonce));
 	// for (i = 0; i < 4; i++) {
 	// 	printf("%x ", nonce[i]);
