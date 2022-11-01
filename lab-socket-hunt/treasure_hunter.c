4a2e4377451fae240625cd789e511abb6bceef1c
chris-luangrath <thechrisluangrath@gmail.com>
2022-10-31 22:01:49 -0600

    level 1

--- lab-socket-hunt/treasure_hunter.c
+++ lab-socket-hunt/treasure_hunter.c
@@ -81,7 +81,7 @@ int main(int argc, char *argv[]) {
 	memcpy(&send_buf[6], &seed, SEED_SIZE);
 	// printf("hey4\n");
 	// 
-	// print_bytes(send_buf,SEND_SIZE);
+	print_bytes(send_buf,SEND_SIZE);
 
 	// pre-socket
 
