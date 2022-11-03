1ed21e053187cb242d69a3a9689d09ab7c1d10d1
chris-luangrath <thechrisluangrath@gmail.com>
2022-11-02 20:01:33 -0600

    lvl3

--- lab-socket-hunt/treasure_hunter.c
+++ lab-socket-hunt/treasure_hunter.c
@@ -228,9 +228,9 @@ int main(int argc, char *argv[]) {
 				// int m = (int)ntohl(par);
 				unsigned short m = 0;
 				// m = par;
-				m = ntohl(par);
+				// m = ntohl(par);
 				// m = ntohs((unsigned short)par);
-				// m = ntohs(par);
+				m = ntohs(par);
 				printf("m=%hu",m);
 				unsigned int sum = 0;
 				for (int j = 0; j < m; j++){
