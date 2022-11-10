ee1fe31df5d34d2a51958d43ef125fbc00f54b59
chris-luangrath <thechrisluangrath@gmail.com>
2022-11-10 09:56:05 -0700

    sending new req

--- lab-proxy-threadpool/proxy.c
+++ lab-proxy-threadpool/proxy.c
@@ -184,8 +184,8 @@ int open_sfd(char* hostname, char* port) {
 	hints.ai_family = AF_INET;    /* Allow IPv4, IPv6, or both, depending on
 				    what was specified on the command line. */
 	hints.ai_socktype = SOCK_STREAM; /* Datagram socket */
-	// hints.ai_flags = AI_PASSIVE;
-	hints.ai_flags = 0;
+	hints.ai_flags = AI_PASSIVE;
+	// hints.ai_flags = 0;
 	hints.ai_protocol = 0;  /* Any protocol */
 	hints.ai_canonname = NULL;
 	hints.ai_addr = NULL;
@@ -232,8 +232,8 @@ int open_sfd(char* hostname, char* port) {
 	// 	local_addr_len = sizeof(ipv6addr);
 	// }
 	
-	// if (bind(sfd, local_addr, local_addr_len) < 0) {
-	if (bind(sfd, result->ai_addr, result->ai_addrlen) < 0) {
+	if (bind(sfd, local_addr, local_addr_len) < 0) {
+	// if (bind(sfd, result->ai_addr, result->ai_addrlen) < 0) {
 		perror("Could not bind");
 		exit(EXIT_FAILURE);
 	}
