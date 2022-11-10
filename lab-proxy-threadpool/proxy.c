76ecff37d176d6d6382077bc9c0ce19281f1997c
chris-luangrath <thechrisluangrath@gmail.com>
2022-11-10 10:19:29 -0700

    sending new req2

--- lab-proxy-threadpool/proxy.c
+++ lab-proxy-threadpool/proxy.c
@@ -34,8 +34,8 @@ int main(int argc, char* argv[])
 	// printf("%s\n", user_agent_hdr);
 	int sfd = 0;
 	int clientsfd = 0;
-	// sfd = open_sfd(NULL, argv[1]);
-	sfd = open_sfd("localhost", argv[1]);
+	sfd = open_sfd(NULL, argv[1]);
+	// sfd = open_sfd("localhost", argv[1]);
 	// printf("sfd=%d\n",sfd);
 	while(1){
 		// accept(sfd,&remote_addr,&remote_addr_len);
@@ -183,7 +183,7 @@ int open_sfd(char* hostname, char* port) {
 	memset(&hints, 0, sizeof(struct addrinfo));
 	hints.ai_family = AF_INET;    /* Allow IPv4, IPv6, or both, depending on
 				    what was specified on the command line. */
-	hints.ai_socktype = SOCK_STREAM; /* Datagram socket */
+	hints.ai_socktype = SOCK_STREAM; /* socket stream */
 	hints.ai_flags = AI_PASSIVE;
 	// hints.ai_flags = 0;
 	hints.ai_protocol = 0;  /* Any protocol */
@@ -345,7 +345,8 @@ void handle_client(int acceptsfd){
 	memset(&hints, 0, sizeof(struct addrinfo));
 	hints.ai_family = AF_INET;    /* Allow IPv4, IPv6, or both, depending on
 				    what was specified on the command line. */
-	hints.ai_socktype = SOCK_STREAM; /* Datagram socket */
+	hints.ai_socktype = SOCK_DGRAM; /* socket stream*/
+	// hints.ai_socktype = SOCK_STREAM; /* socket stream*/
 	hints.ai_flags = 0;
 	hints.ai_protocol = 0;  /* Any protocol */
 	hints.ai_canonname = NULL;
