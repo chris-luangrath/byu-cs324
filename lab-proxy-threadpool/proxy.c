dd5e22e01a1f5041bc7a337e42ea4e3993f351ad
chris-luangrath <thechrisluangrath@gmail.com>
2022-11-10 00:57:55 -0700

    sending new req

--- lab-proxy-threadpool/proxy.c
+++ lab-proxy-threadpool/proxy.c
@@ -1,8 +1,8 @@
+#include <netdb.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
-#include <stdlib.h>
-#include <netdb.h>
 #include <sys/types.h>
 #include <unistd.h>
 
