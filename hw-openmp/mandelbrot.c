3f1b2e677069d66a551d5ca65a356d5ae793214a
chris-luangrath <thechrisluangrath@gmail.com>
2022-11-17 01:50:13 -0700

    part 3

--- hw-openmp/mandelbrot.c
+++ hw-openmp/mandelbrot.c
@@ -112,7 +112,7 @@ int main(int argc, char* argv[])
 
   double start = omp_get_wtime();
 
-  #pragma omp parallel for private(i,k) firstprivate(j)
+  #pragma omp parallel for private(i,k) firstprivate(saved)
   for (j = 0; j < yres; j++) {
     y = ymax - j * dy;
     for(i = 0; i < xres; i++) {
